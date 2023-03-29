aa8565fa320ac5250953e97db84123825602dc86
DevMasterSh <dev_master_sh@mail.ru>
2023-03-29 13:37:39 +0300

    Solution DZ_5

---
+++ DZ_5/task1.java
@@ -0,0 +1,68 @@
+﻿/* Реализуйте структуру телефонной книги с помощью HashMap, учитывая,
+что 1 человек может иметь несколько телефонов.
+Добавить функции 1) Добавление номера
+2) Вывод всего
+Пример:
+Я ввожу: 1
+К: Введите фамилию
+Я: Иванов
+К: Введите номер
+Я: 1242353
+К: Введите 1) Добавление номера
+2) Вывод всего
+Я ввожу: 1
+К: Введите фамилию
+Я: Иванов
+К: Введите номер
+Я: 547568
+К: Введите 1) Добавление номера
+2) Вывод всего
+Я: 2
+Иванов: 1242353, 547568 */
+
+package DZ_5;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Map<String, LinkedList<String>> phonesBook = new HashMap<>();
+        Scanner iScanner = new Scanner(System.in);
+        String str = "Выберите необходимый пункт меню:\n 1 - Добавление номера\n 2 - Вывод всего\n 0 - выход";
+        System.out.println(str);
+        int nav = iScanner.nextInt();
+        if (nav != 0) {
+            for (int i = nav; i != 0; i = iScanner.nextInt()) {
+                switch (i) {
+                    case 1:
+                        System.out.println("Введите фамилию: ");
+                        String surname = iScanner.next();
+                        System.out.println("Введите телефон: ");
+                        String phone = iScanner.next();
+                        if (phonesBook.containsKey(surname)) {
+                            phonesBook.get(surname).add(phone);
+                        } else {
+                            LinkedList<String> phones = new LinkedList<>();
+                            phones.add(phone);
+                            phonesBook.put(surname, phones);
+                        }
+                        break;
+                    case 2:
+                        for (var item : phonesBook.entrySet()) {
+                            System.out.println(
+                                    item.getKey() + ": " + item.getValue().toString().replaceAll("[ \\[ \\] ]", ""));
+                        }
+                        break;
+                    default:
+                        System.out.println("Такого пункта в меню нет.");
+                        break;
+                }
+                System.out.println("\n" + str);
+            }
+        }
+        iScanner.close();
+    }
+}
---
+++ DZ_5/task2.java
@@ -0,0 +1,52 @@
+﻿package DZ_5;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class task2 {
+
+    public static void main(String[] args) throws IOException {
+        String[] people = new String[] { "Иван Иванов", "Светлана Петрова", "Кристина Белова", "Анна Мусина",
+                "Анна Крутова", "Иван Юрин", "Петр Лыков", "Павел Чернов", "Петр Чернышов", "Мария Федорова",
+                "Марина Светлова", "Мария Савина", "Мария Рыкова", "Марина Лугова", "Анна Владимирова", "Иван Мечников",
+                "Петр Петин", "Иван Ежов" };
+        Map<String, String> listPeople = new TreeMap<>();
+        Map<String, Integer> sortNames = new LinkedHashMap<>();
+        int count = 0;
+        for (int i = 0; i < people.length; i++) {
+            String[] name = people[i].split(" ");
+            ArrayList<String> names = new ArrayList<>();
+            for (int j = 0; j < name.length; j++) {
+                names.add(name[j]);
+            }
+            listPeople.put(names.get(1), names.get(0));
+        }
+
+        for (var map : listPeople.entrySet()) {
+            for (var item : listPeople.entrySet()) {
+                if (map.getValue().equals(item.getValue())) {
+                    count++;
+                    if (count > 1) {
+                        listPeople.remove(item.getValue());
+                    }
+
+                    sortNames.put(map.getValue(), count);
+                }
+            }
+            count = 0;
+        }
+
+        for (int i = sortNames.size(); i > 0; i--) {
+            for (var element : sortNames.entrySet()) {
+                if (element.getValue() == i) {
+                    System.out.printf("%s: %d\n", element.getKey(), element.getValue());
+                }
+
+            }
+        }
+
+    }
+}
